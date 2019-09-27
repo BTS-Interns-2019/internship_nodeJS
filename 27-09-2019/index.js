05bdcce432b39dd88c764661df103b120ea46323
Luis <luiscarlosg.bts@gmail.com>
2019-09-27 14:50:20 -0500

    servicio

---
+++ 27-09-2019/index.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+'use strict'
+const http = require('http');
+//
+let app = http.createServer((req, res) => {
+    fs.readFile('example.txt', 'utf8', function (err, data) {
+        if (err) {
+            res.writeHead(400, { 'Content-Type': 'text/plain' });
+            res.end('Hubo un error')
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end(data)
+        }
+    });
+})
+
+app.listen(5000, '127.0.0.1');
+console.log('Node server running on port 5000')
