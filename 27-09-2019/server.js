4ec2e0fe60a4cfa063a4dcd1daa97249730d7eb3
Urbano <urbano.gonzalez@alumnos.udg.mx>
2019-09-27 14:15:50 -0500

    Config a server on node

---
+++ 27-09-2019/server.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const fs = require ('fs');
+
+let app = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-type': 'text/plain'});
+    const txt= fs.readFile('../example.txt','utf-8',(err, data) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log(data);
+        
+        res.end(data);
+    })
+});
+
+app.listen(5000, '127.0.0.1');
+console.log('Node server is running on port 5000');
