2086b0b9a24aff10aa2a7471470788bcde601dcd
Hector Soto <hectors.bts@gmail.com>
2019-09-27 14:10:36 -0500

    Move comment

--- 27-09-2019/exercise1.js
+++ 27-09-2019/exercise1.js
@@ -6,21 +6,21 @@ const fs = require('fs');
 
 // create an instance of the http server to handle HTTP requests
 const app = http.createServer((req, res) => {
-  // write the response
   fs.readFile('../example.txt', (err, data) => {
     if (err) {
       res.writeHead(400, {
         'Content-Type': 'json/application',
       });
-
+      
       return res.end(JSON.stringify(err));
     }
-
+    
     // writing the status code and headers
     res.writeHead(200, {
       'Content-Type': 'text/plain',
     });
-
+    
+    // write the response
     return res.end(data.toString());
   });
 });
