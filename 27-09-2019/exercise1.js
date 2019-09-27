0fcec5db8a90fcdf3aea3382fb4c5a2143b55c90
ViryDe <virid.bts@gmail.com>
2019-09-27 14:51:47 -0500

    Ejericio en clase, leyendo archivo

--- 27-09-2019/exercise1.js
+++ 27-09-2019/exercise1.js
@@ -6,11 +6,11 @@ const fs = require("fs");
 //create a instance of the http server to handle http request
 let app = http.createServer((req, res) => {
   res.writeHead(200, { "Content-type": "text/plain" });
-  fs.readFile("./example.txt", () => {
-    res.end();
+  fs.readFile("./example.txt", (data) => {
+    res.end(data);
   });
 });
 
-//Start the server on port 5000
+//Start the server on port 300
 app.listen(3000, "127.0.0.1");
 console.log("Node server running on port 3000");
