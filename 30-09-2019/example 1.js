e559b57dc634ddc5766b39a5bd7ba40fb21dd8f0
Miguel <miguelv.bts@gmail.com>
2019-09-30 14:10:49 -0500

    Current status of the example 1

---
+++ 30-09-2019/example 1.js
@@ -0,0 +1,20 @@
+let http = require('http');
+let fs = require('fs');
+
+let app = http.createServer((req, res)=>{
+    res.writeHead(200,{'Content-type':'application/json'});
+    let data = fs.readFile('./db.json',(err) => {
+        if(err){
+            return 'a descriptive message';
+        }
+    });
+    console.log(data);
+    if(data==undefined){
+        res.end('a descriptive message')
+    }else{
+        console.log('file readed');
+        res.end(data);
+    }
+})
+app.listen(5000,'127.0.0.1');
+console.log("Conexion establecida")
