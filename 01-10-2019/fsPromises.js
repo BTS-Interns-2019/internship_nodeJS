8d4e9e965bb0ce27254ef78f2d60619a7be44002
Hector Soto <hectors.bts@gmail.com>
2019-10-01 16:40:18 -0500

    Finish v1 of endpoints

--- 01-10-2019/fsPromises.js
+++ 01-10-2019/fsPromises.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 
-// function that returns a promise
+// functions that return a promise
+
 function readFile(path) {
   return new Promise((resolve, reject) => {
     fs.readFile(path, (err, data) => {
@@ -11,6 +12,56 @@ function readFile(path) {
       resolve(data);
     });
   });
-} 
+}
 
-module.exports = readFile;
+function writeFile(path, data) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(path, data, (err) => {
+      if (err) {
+        reject(err);
+      }
+
+      resolve(data);
+    });
+  });
+}
+
+function checkFile(path) {
+  return new Promise((resolve, reject) => {
+    fs.access(path, fs.F_OK, (err) => {
+      if (err) {
+        reject(err);
+      }
+
+      resolve('File exists');
+    });
+  });
+}
+
+function deleteFromFile(originalData, newData) {
+  const keys = Object.keys(newData);
+
+  keys.forEach((key) => {
+    if (
+      originalData.hasOwnProperty(key)
+      && typeof originalData[key] === 'boolean'
+      && !originalData[key]
+      && typeof newData[key] === 'boolean'
+      && newData[key]
+    ) {
+      delete originalData[key];
+    }
+  });
+
+  return JSON.stringify(originalData);
+}
+
+function BreakChain() {}
+
+module.exports = {
+  readFile,
+  writeFile,
+  checkFile,
+  deleteFromFile,
+  BreakChain,
+};
--- 01-10-2019/index.js
+++ 01-10-2019/index.js
@@ -3,7 +3,13 @@
 // modules
 const bodyParser = require('body-parser');
 const express = require('express');
-const readFile = require('./fsPromises');
+const {
+  readFile,
+  writeFile,
+  checkFile,
+  deleteFromFile,
+  BreakChain,
+} = require('./fsPromises');
 
 // create express app
 const app = express();
@@ -19,6 +25,7 @@ function setResponse(res, headers, status, data) {
 }
 
 // set routes and methods to listen
+// get
 app.get('/', (req, res) => {
   readFile('./db.json')
     .then((data) => {
@@ -31,6 +38,54 @@ app.get('/', (req, res) => {
         setResponse(res, { 'Content-Type': 'application/json' }, 400, err);
       }
     });
+});
+
+// post
+app.post('/', (req, res) => {
+  writeFile('./db.json', JSON.stringify(req.body))
+    .then((data) => {
+      setResponse(res, { 'Content-Type': 'application/json' }, 201, data);
+    })
+    .catch((err) => {
+      setResponse(res, { 'Content-Type': 'application/json' }, 400, err);
+    });
+});
+
+// put
+app.put('/', (req, res) => {
+  const path = './db.json';
+
+  // begin to check if file exists
+  checkFile(path)
+    .then(() => writeFile(path, JSON.stringify(req.body)))
+    .catch((err) => {
+      setResponse(res, { 'Content-Type': 'appplication/json' }, 404, err);
+      // throw anything just to break out of the chain
+      throw new BreakChain();
+    })
+    .then((data) => setResponse(res, { 'Content-Type': 'application/json' }, 200, data))
+    .catch((err) => setResponse(res, { 'Content-Type': 'appplication/json' }, 400, err));
+});
+
+// delete
+app.delete('/', (req, res) => {
+  const path = './db.json';
+
+  // begin by checking if file exists
+  checkFile(path)
+    // first read the file
+    .then(() => readFile(path))
+    .catch((err) => {
+      setResponse(res, { 'Content-Type': 'appplication/json' }, 404, err);
+      // throw anything just to break out of the chain
+      throw new BreakChain();
+    })
+    // delete the properties from the content
+    .then((data) => deleteFromFile(JSON.parse(data.toString()), req.body))
+    // write the modified data
+    .then((data) => writeFile(path, data))
+    .then((data) => setResponse(res, { 'Content-Type': 'application/json' }, 200, data))
+    .catch((err) => setResponse(res, { 'Content-Type': 'appplication/json' }, 400, err));
 });
 
 app.listen(port, () => {
