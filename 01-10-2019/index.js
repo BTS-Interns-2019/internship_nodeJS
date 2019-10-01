3b98b4bccdc27e819b4cc391130ad423e0117cb4
Ricardo Martin del Campo Loza <ricardom.bts@gmail.com>
2019-10-01 13:27:13 -0500

    agrego comentarios

--- 01-10-2019/index.js
+++ 01-10-2019/index.js
@@ -4,18 +4,18 @@ const express = require ('express');
 const fs = require('fs');
 const files = require('./files')
 
-const app = express();
+const app = express(); // se crea el servidor
 const port = 5000;
 
 app.get('/', (req, res)=>{  //si es peticion GET y se hace desde la raíz hace lo siguiente
 
-    fs.readFile('./db.json', 'utf-8',(err, data)=>{
+    fs.readFile('./db.json', 'utf-8',(err, data)=>{// se lee el archivo
 
-        if(data == undefined || data.length == 0){
+        if(data == undefined || data.length == 0){ //si el archivo está vacío 
             res.set('Content-Type', 'text/plain');
             res.status(404);
             res.send('El archivo no existe o está vacío');
-        }else{
+        }else{                                      // si el archivo tien contenido va a enviarlo con un status 200 como json
             res.set('Content-Type', 'application/json');
             res.status(200);
             res.send(data);
@@ -25,6 +25,6 @@ app.get('/', (req, res)=>{  //si es peticion GET y se hace desde la raíz hace l
 
 });
 
-app.listen(port, ()=>{
+app.listen(port, ()=>{ // el servidor estará escuchando en al puerto 5000
     console.log(`example app listening on ${port}`);
 })
