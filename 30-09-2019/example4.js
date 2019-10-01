6ae5ed9e62ab1c47f22709178ca0892ed46dbf76
Marlon Torres <marlont.bts@gmail.com>
2019-10-01 01:19:50 -0500

    examples validados y finalizados

--- 30-09-2019/example4.js
+++ 30-09-2019/example4.js
@@ -4,9 +4,8 @@ const fs = require('fs');
 
 // Create an instance of the http server to handle HTTP requests
 const app = http.createServer((req, res) => {
-  if(req.method === 'PUT'){
+  if(req.method === 'DELETE') {
     let body = [];
-
     req.on('data', (chunk) => {
       body.push(chunk);
     });
@@ -15,17 +14,18 @@ const app = http.createServer((req, res) => {
       body = Buffer.concat(body).toString();
       body = JSON.parse(body);
 
-      let file = fs.readFileSync('./db.json', {encoding: 'utf-8'});
-      let read = JSON.parse(file);
+      if(Object.values(body)[0]){
+        let file = fs.readFileSync('./db.json');
+        file = JSON.parse(file);
 
-      if(read.hasOwnProperty(Object.keys(body)[0])) {
-        read[Object.keys(body)[0]] = Object.values(body)[0];
-        fs.writeFileSync('./db.json', JSON.stringify(read, null, 2));
+        delete file[Object.keys(body)[0]];
+        
+        fs.writeFileSync('./db.json', JSON.stringify(file, null, 2));
       }
     });
-    
-    let file2 = fs.readFileSync('./db.json');
-    res.end(file2);
+
+    let read = fs.readFileSync('./db.json');
+    res.end(read);
   }
 });
 
