a82de431af793095bc2aee361f4617dce41d34ea
ViryDe <virid.bts@gmail.com>
2019-10-02 18:21:48 -0500

    Arreglando error en BodyParse

--- futbol-2/index.js
+++ futbol-2/index.js
@@ -8,7 +8,7 @@ const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({extended: false}))
-app.bodyParser.json();
+app.use(bodyParser.json());
 
 app.post('/login', (req, res) => {
     login()
