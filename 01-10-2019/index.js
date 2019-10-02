0e11b95a0d97fb55610668eae58c51e2e2676067
estefaniacr <estefaniac.bts@gmail.com>
2019-10-01 23:39:35 -0500

    post, put y delete

--- 01-10-2019/index.js
+++ 01-10-2019/index.js
@@ -8,7 +8,11 @@ const fs = require('fs');
 const app = express();
 const port = 5000;
 
-app.get('/', (req, res) => {
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+
+
+app.get('/get', (req, res) => {
   fs.stat('./db.json', (err, data) => {
     if (err) {
       res.set('Content-Type', 'text/plain');
@@ -29,6 +33,60 @@ app.get('/', (req, res) => {
       })
     }
   })
+});
+
+app.post('/post', (req, res) => {
+  fs.writeFile('./db.json', JSON.stringify(req.body), (err, data) => {
+    if (err) {
+      res.set('Content-Type', 'application/json');
+      res.status(400).send('Unexpected error');
+    } else {
+      res.set('Content-Type', 'application/json');
+      res.status(201).send(req.body);
+    }
+  })
+})
+
+app.put('/put', (req, res) => {
+  fs.stat('./db.json', (err, data) => {
+    if (err) {
+      res.set('Content-Type', 'text/plain');
+      res.status(404).send('File db.json does not exist');
+    } else { 
+      fs.writeFile('./db.json', JSON.stringify(req.body), (err, data) => {
+        if (err) {
+          res.set('Content-Type', 'application/json');
+          res.status(400).send('Unexpected error');
+        } else {
+          res.set('Content-Type', 'application/json');
+          res.status(201).send(req.body);
+        }
+      })
+    }
+  })
+});
+
+app.delete('/delete', (req, res) => {
+  fs.readFile('./db.json', 'utf8', (err, data) => {
+    if (err) {
+      res.status(404).send('File doesn´t exist');
+    }
+    data = JSON.parse(data);
+    if (Object.values(data).length < 0) {  
+      res.status(404).send('The property does not exist on db.json');
+    } else {
+      if (data.hasOwnProperty(Object.keys(req.body)) && Object.values(req.body)[0] === 'true') {
+        delete data[Object.keys(req.body)];
+    };
+    fs.writeFile('./db.json', JSON.stringify(data), (err) => {
+        if (err) {
+          res.status(400).send('Error on write file');
+        } else {
+          res.set('content-type', 'application/json').status(200).send(data);
+        }
+      });
+    };
+  });
 });
 
 app.listen(port, () => {
