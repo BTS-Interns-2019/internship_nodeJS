55cb2cd50ebfbfe7eb99794a9c6dd186166dfc1f
Miguel <miguelv.bts@gmail.com>
2019-10-02 19:05:06 -0500

    Post method added

--- 02-10-2019/index.js
+++ 02-10-2019/index.js
@@ -2,9 +2,13 @@
 
 const express = require('express');
 const film = require('./userDB');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 5000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/',(req, res)=>{
     film.getAllUsers()
@@ -13,6 +17,17 @@ app.get('/',(req, res)=>{
         res.send(record);
     });
 });
+
+app.post('/',(req, res)=>{
+    film.postNewUser(req)
+    .then((record)=>{
+        res.set('Content-Type','application/json');
+        res.send(record);
+    })
+    .catch((err)=>{
+        console.error(err);
+    })
+})
 
 app.listen(port,()=>{
     console.log(`Init connection, listening in port ${port}`);
--- 02-10-2019/userDB.js
+++ 02-10-2019/userDB.js
@@ -31,4 +31,38 @@ function getAllUsers(){
         })
     })
 }
-module.exports = {getFirstUser, getAllUsers};
+
+function postNewUser(request){
+    return new Promise((resolve, reject)=>{
+        db.getConnection(function(err,connection){
+            if(err){reject('DB Connection error');}
+            
+            let sql = `INSERT INTO film 
+            (title, description, 
+            release_year, language_id, 
+            rental_duration,rental_rate, 
+            length, replacement_cost, 
+            rating, special_features, 
+            last_update) 
+            VALUES 
+            ('${request.body.title}',
+            '${request.body.description}',
+            '${request.body.release_year}',
+            '${request.body.language_id}',
+            '${request.body.rental_duration}',
+            '${request.body.rental_rate}',
+            '${request.body.length}',
+            '${request.body.replacement_cost}',
+            '${request.body.rating}',
+            '${request.body.special_features}',
+            '${request.body.last_update}')`;
+
+            connection.query(sql, function (error,result){
+                    if(error)throw error;
+                    resolve("1 row added");
+                }
+            );
+        })
+    })
+}
+module.exports = {getFirstUser, getAllUsers, postNewUser};
