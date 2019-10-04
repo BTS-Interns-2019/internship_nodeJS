28081ba42c1f40319e26a0f61ec93817f002841b
Ricardo Martin del Campo Loza <ricardom.bts@gmail.com>
2019-10-04 01:52:24 -0500

    variables fixed

--- futbol-2/config/constants.js
+++ futbol-2/config/constants.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 module.exports = {
   DB_HOST: process.env.DB_HOST,
   DB_USER: process.env.DB_USER,
-  DB_PASSSWORD: process.env.DB_PASSSWORD,
+  DB_PASSWORD: process.env.DB_PASSWORD,
   DB_NAME: process.env.DB_NAME,
   APP_PORT: process.env.APP_PORT,
   ENV: process.env.ENV
--- futbol-2/config/db.js
+++ futbol-2/config/db.js
@@ -3,12 +3,12 @@
 const mysql = require("mysql");
 const config = require('./constants')
 
-
+console.log(config)
 let serverConnection = mysql.createPool({
   host: config.DB_HOST,
   port: config.APP_POR,
   user: config.DB_USER,
-  password: config.DB_PASSSWORD,
+  password: config.DB_PASSWORD,
   database: config.DB_NAME,
 
 });
