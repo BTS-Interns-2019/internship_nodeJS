b34f5f35b524f51349d5c12049feffb3d8191b86
ViryDe <virid.bts@gmail.com>
2019-10-04 00:19:10 -0500

    Agregando cambios en getUser y filter

--- futbol-2/daos/getUserLogin.js
+++ futbol-2/daos/getUserLogin.js
@@ -15,19 +15,7 @@ function postUser(body){
 
             if(error){throw error};
             // console.log(results[0].password, body.password);
-            compare(body.password, results[0].password)
-            .then((data) => {
-
-                var tokenData = body 
-
-                var token = jwt.sign(tokenData, 'Secret Password', {
-                    expiresIn: 60 * 60 * 24 // expires in 24 hours
-                 })
-
-
-                resolve(token);
-            })
-            .catch( (data) => reject(data));
+            
 
             })
 
--- futbol-2/filters/tokenValidator.js
+++ futbol-2/filters/tokenValidator.js
@@ -7,8 +7,22 @@ const logger = require('log4js');
 // * @param {Object} res - client response in case toke is invalid or expired
 // * @param {Object} next - method to continue
 
-function tokenValidator(req, res, next) {
-  logger.debug('token validator');
+function tokenValidator(bpassword, res, next) {
+    return compare(bpassword, results[0].password)
+            .then((data) => {
+
+                var tokenData = body 
+
+                var token = jwt.sign(tokenData, 'Secret Password', {
+                    expiresIn: 60 * 60 * 24 // expires in 24 hours
+                 })
+
+
+                resolve(token);
+            })
+            .catch( (data) => reject(bpassword));
+            
+//   logger.debug('token validator');
 }
 
 module.exports = tokenValidator;
