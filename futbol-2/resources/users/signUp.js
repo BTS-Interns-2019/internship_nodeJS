b982e040cbe70ff5ce296f03ccfe5c07defc686f
Luis <luiscarlosg.bts@gmail.com>
2019-10-03 23:55:11 -0500

    carpeta en services

---
+++ futbol-2/resources/users/signUp.js
@@ -0,0 +1,26 @@
+/**
+ * 
+ * @param {user} user JSON del usuario a agregar
+ */
+function addUser(user) {
+    return new Promise((resolve, reject) => {
+        db.getConnection(function (err, connection) {
+            if (err) {
+                reject('DB connection error', err)
+            }
+            bcrypt
+                .hash(user.password, 5)
+                .then(res => {
+                    user.password = res
+                    connection.query(`INSERT INTO user (email, password) values ('${user.email}','${user.password}')`, function (error, results, fields) {
+                        connection.release();
+                        if (error) {
+                            throw error
+                        }
+                        resolve(user)
+                    })
+                })
+                .catch(err => console.error(err.message));
+        })
+    })
+}
