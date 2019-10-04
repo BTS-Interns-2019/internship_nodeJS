990ad48dc7b039bc409d6d71e6b9f176c01e0c17
ViryDe <virid.bts@gmail.com>
2019-10-03 23:15:26 -0500

    Agregando archivo constant.js

---
+++ futbol-2/config/constants.js
@@ -0,0 +1,10 @@
+"use strict";
+
+module.exports = {
+  DB_HOST: process.env.DB_HOST,
+  DB_USER: process.env.DB_USER,
+  DB_PASSSWORD: process.env.DB_PASSSWORD,
+  DB_NAME: process.env.DB_NAME,
+  APP_PORT: process.env.APP_PORT,
+  ENV: process.env.ENV
+};
